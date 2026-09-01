3f98311cb8a6db589e023c23010a1455f6614147
a756446240 <756446240@qq.com>
2026-09-01 09:07:28 +0800

    add ios/Flutter/flutter_export_environment.sh

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=C:\dev\flutter_sdk\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\Administrator\WorkBuddy\2026-08-31-09-39-25\taobao_src\taobao_clone"
+export "FLUTTER_FRAMEWORK_SWIFT_PACKAGE_PATH=C:\Users\Administrator\WorkBuddy\2026-08-31-09-39-25\taobao_src\taobao_clone\ios\Flutter\ephemeral\Packages\.packages\FlutterFramework"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.3.6"
+export "FLUTTER_BUILD_NUMBER=11"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
