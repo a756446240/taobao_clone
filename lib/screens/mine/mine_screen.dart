b7206ba290728c730cd8668d565954c1181d1a23
a756446240 <756446240@qq.com>
2026-09-18 12:06:16 +0800

    chore: settings version 1.9.145

--- lib/screens/mine/mine_screen.dart
+++ lib/screens/mine/mine_screen.dart
@@ -1064,7 +1064,7 @@ class _MineScreenState extends State<MineScreen> {
   /// 单击"设置" → 设置页（双击仍是编辑资料）
   void _openSettings() {
     Navigator.of(context).push(
-      MaterialPageRoute(builder: (_) => const SettingsScreen(version: '1.9.144')),
+      MaterialPageRoute(builder: (_) => const SettingsScreen(version: '1.9.145')),
     );
   }
 
